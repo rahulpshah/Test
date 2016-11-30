b7e90a0f26ce645db3a87ceff76ba719a9766674
Rahul P Shah <rshah5@ncsu.edu>
2016-11-30 01:45:11 -0500

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
                 <h2>Congratulations</h2>
-                <p>Rahul Shah</p>
+                <p>Rahul P Shah</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
